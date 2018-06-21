b1aa0045a2adc55ca425af81b92cbbb011556d52
Dylan Fiedler <dylan.fiedler@ge.com>
2018-06-21 11:28:30 -0500

    longest substring example

--- src/LongestSubstring/LongestSubstring.java
+++ src/LongestSubstring/LongestSubstring.java
@@ -1,14 +1,32 @@
 package LongestSubstring;
 
+import java.util.HashMap;
+
 public class LongestSubstring {
 
+    public int lengthOfLongestSubstring(String s) {
 
+        if (s.length() == 0){
+            return 0;
+        }
 
+        if (s.length() == 1){
+            return 1;
+        }
+        int longestSubstring = traverseSubstring(s);
+        return longestSubstring;
+    }
 
-    
-
-
-    public static void main (String[] args){
-
+    public int traverseSubstring(String s){
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        int max = 0;
+        for (int i = 0, j = 0; i < s.length(); i++){
+            if(hashMap.containsKey(s.charAt(i))){
+                j = Math.max(j,hashMap.get(s.charAt(i))+1);
+            }
+            hashMap.put(s.charAt(i),i);
+            max = Math.max(max,i-j+1);
+        }
+        return max;
     }
 }
